491d093bd40d1614d2c92d58bf5a0f8a97bb6bad
Nethmi Sansala <nethu0332264106@gmail.com>
2025-07-28 14:32:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '🚀 Hello from Flask app deployed with GitHub Actions and AWS ECS.our group members are chathuni, nethmi and hiruni!!!'
+    return '🚀 Hello from Flask app deployed with GitHub Actions and AWS ECS.our group members are chathuni, nethmi and hiruni!!!!!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
